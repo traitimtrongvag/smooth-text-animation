4cee6320c72d9b5e7752f1af9f41efefe1ec3b2e
traitimtrongvag <pan2512811@gmail.com>
2026-03-01 16:54:32 +0000

    feat: export 12 new animation functions, bump version to 0.1.2

--- smooth_text_animation/__init__.py
+++ smooth_text_animation/__init__.py
@@ -3,7 +3,7 @@
 Smooth Text Animation - Beautiful text animations for terminal
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __author__ = "traitimtrongvag"
 __email__ = "tbinh831@gmail.com"
 
@@ -18,6 +18,18 @@ from .animations import (
     reverse_text,
     rotate_text,
     combined_animation_simultaneous,
+    glitch_text,
+    rainbow_text,
+    matrix_reveal,
+    typewriter_advanced,
+    bounce_text,
+    scramble_solve,
+    slide_in,
+    pulse_text,
+    reveal_mask,
+    zigzag_text,
+    expanding_center,
+    neon_flicker,
 )
 
 __all__ = [
@@ -31,4 +43,16 @@ __all__ = [
     "reverse_text",
     "rotate_text",
     "combined_animation_simultaneous",
+    "glitch_text",
+    "rainbow_text",
+    "matrix_reveal",
+    "typewriter_advanced",
+    "bounce_text",
+    "scramble_solve",
+    "slide_in",
+    "pulse_text",
+    "reveal_mask",
+    "zigzag_text",
+    "expanding_center",
+    "neon_flicker",
 ]
